c20ce77eea1a81448e2473dc1fdc0c564a33ca01
Julian <julian31186@gmail.com>
2024-04-22 15:11:03 -0400

    djik and bf descriptions

---
+++ templates/bellman_ford.py
@@ -0,0 +1,8 @@
+'''
+
+Used to find the shortest path from start node, to all other nodes within the graph (works with negative edges, however we CANNOT have a negative cycle)
+
+Returns the a 2D matrix that represents cost to get from start node to any other node within the graph
+
+
+'''
---
+++ templates/djik.py
@@ -0,0 +1,9 @@
+'''
+Used to find the shortest path from start node, to all other nodes within the graph (will not work with negative edges)
+
+Returns the weight map of costs from start node -> other nodes
+
+
+TODO: Include path map with traversal for path 
+
+'''
